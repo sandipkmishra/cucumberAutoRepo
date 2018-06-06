69c742624a0047169d92a2e659822db177de663b
sandipkmishra <39923620+sandipkmishra@users.noreply.github.com>
2018-06-06 21:27:44 +0530

    Update RunDataDrivenTest.java

--- SampleBDDFramework/src/test/java/com/facebook/runners/RunDataDrivenTest.java
+++ SampleBDDFramework/src/test/java/com/facebook/runners/RunDataDrivenTest.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 		features="src/test/resources/com/facebook/features/datadriven/",
 		glue= {"com.facebook.step_definitions.datadriven"},
 		plugin= {"html:target/cucumber-html-report","json:target/cucumber-json-report/cucumber2.json","pretty:target/cucumber-pretty.txt",
-				"usage:target/cucumber-usage.json","junit:target/cucumber-results.xml","com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"}
+				"junit:target/cucumber-results.xml","com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"}
 		)
 public class RunDataDrivenTest {
 
