188e1668e0b81b85c56a302909a89ca1e8d5f12c
sandipkmishra <39923620+sandipkmishra@users.noreply.github.com>
2018-06-06 21:30:36 +0530

    Update RunShareDataTest.java

--- SampleBDDFramework/src/test/java/com/facebook/runners/RunShareDataTest.java
+++ SampleBDDFramework/src/test/java/com/facebook/runners/RunShareDataTest.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 		features="src/test/resources/com/facebook/features/sharedata/",
 		glue= {"com.facebook.step_definitions.sharedata"},
 		plugin= {"html:target/cucumber-html-report","json:target/cucumber-json-report/cucumber8.json","pretty:target/cucumber-pretty.txt",
-				"usage:target/cucumber-usage.json","junit:target/cucumber-results.xml","com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"}
+				"junit:target/cucumber-results.xml","com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"}
 		)
 public class RunShareDataTest {
 
